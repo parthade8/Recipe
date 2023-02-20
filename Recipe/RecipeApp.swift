92a05c117e3702bc32509393a0cd35ce971dfb92
parthade <parthade@deloitte.com>
2023-02-20 17:24:27 +0530

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by De, Partha on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
